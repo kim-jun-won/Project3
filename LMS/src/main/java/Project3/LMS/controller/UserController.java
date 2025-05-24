9eee8a276dca019a66050c76713f52a9951138ea
홍왕기 <144398371+hongwangki@users.noreply.github.com>
2025-05-24 13:35:42 +0900

    2025/05-24 준원+현민 코드 홍왕기 코드와 병합

--- LMS/src/main/java/Project3/LMS/controller/UserController.java
+++ LMS/src/main/java/Project3/LMS/controller/UserController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
@@ -276,6 +278,37 @@ public class UserController {
         }
 
         return "redirect:/mypage/edit";
+    }
+
+    /**
+     * 관리자가 회원 전부를 볼 수 있음
+     */
+    @GetMapping("/user-info")
+    public String showUserInfo(
+            @RequestParam(required = false,name = "userType") String userType,
+            @RequestParam(required = false,name = "keyword") String keyword,
+            @RequestParam(required = false,name="department") String department,
+            Model model) {
+
+        boolean hasSearch = (userType != null && !userType.equals("ALL")) ||
+                (keyword != null && !keyword.isBlank()) ||
+                (department != null && !department.isBlank());
+
+        if (hasSearch) {
+            List<User> resultList = userService.search(userType, keyword, department);
+            model.addAttribute("resultList", resultList);
+            model.addAttribute("title", "검색 결과");
+        } else {
+            model.addAttribute("studentList", studentService.findAll());
+            model.addAttribute("professorList", professorService.findAll());
+            model.addAttribute("title", "전체 사용자 목록");
+        }
+
+        model.addAttribute("userType", userType);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("department", department);
+
+        return "user-info";
     }
 
 }
